89f488d5791741a9485e332e5864796f918d58e4
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-05 19:16:34 +0800

    Added both good and bad contracts

--- re-entrancy/contracts/Re-Entrancy.sol
+++ re-entrancy/contracts/Re-Entrancy.sol
@@ -1,5 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
-contract GoodContract {}
+contract GoodContract {
+    mapping(address => uint) public balances;
+
+    function addBalance() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0, "Nothing to withdraw");
+
+        (bool sent, ) = msg.sender.call{value: balances[msg.sender]}("");
+        require(sent, "Failed to send Eth");
+    }
+}
