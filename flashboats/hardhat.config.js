b4131ed5498a894828e0e16d1ffdb59a913fe684
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-15 22:24:54 +0800

    Passed the tests

--- flashboats/hardhat.config.js
+++ flashboats/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (!RPC_URL) {
+  throw new Error("NO RPC URL FOUND");
+}
+if (!PRIVATE_KEY) {
+  throw new Error("No private key found");
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- flashboats/scripts/deploy.js
+++ flashboats/scripts/deploy.js
@@ -1,31 +1,70 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {
+  FlashbotsBundleProvider,
+} = require("@flashbots/ethers-provider-bundle");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Deploy FakeNFT Contract
+  const fakeNFT = await ethers.getContractFactory("FakeNFT");
+  const FakeNFT = await fakeNFT.deploy();
+  await FakeNFT.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Address of Fake NFT Contract:", FakeNFT.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // Create a Quicknode WebSocket Provider
+  const provider = new ethers.providers.WebSocketProvider(
+    process.env.QUICKNODE_WS_URL,
+    "goerli"
   );
+
+  // Wrap your private key in the ethers Wallet class
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  // Create a Flashbots Provider which will forward the request to the relayer
+  // Which will further send it to the flashbot miner
+  const flashbotsProvider = await FlashbotsBundleProvider.create(
+    provider,
+    signer,
+    // URL for the flashbots relayer
+    "https://relay-goerli.flashbots.net",
+    "goerli"
+  );
+
+  provider.on("block", async (blockNumber) => {
+    console.log("Block Number: ", blockNumber);
+    // Send a bundle of transactions to the flashbot relayer
+    const bundleResponse = await flashbotsProvider.sendBundle(
+      [
+        {
+          transaction: {
+            // ChainId for the Goerli network
+            chainId: 5,
+            // EIP-1559
+            type: 2,
+            // Value of 1 FakeNFT
+            value: ethers.utils.parseEther("0.01"),
+            // Address of the FakeNFT
+            to: FakeNFT.address,
+            // In the data field, we pass the function selctor of the mint function
+            data: FakeNFT.interface.getSighash("mint()"),
+            // Max Gas Fes you are willing to pay
+            maxFeePerGas: BigNumber.from(10).pow(9).mul(3),
+            // Max Priority gas fees you are willing to pay
+            maxPriorityFeePerGas: BigNumber.from(10).pow(9).mul(2),
+          },
+          signer: signer,
+        },
+      ],
+      blockNumber + 1
+    );
+
+    // If an error is present, log it
+    if ("error" in bundleResponse) {
+      console.log(bundleResponse.error.message);
+    }
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
